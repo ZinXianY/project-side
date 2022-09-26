b62f381d58d62d1e90cfb168406e088d0e977eb9
Zinyang <tyulkeds159357@gmail.com>
2022-09-26 18:16:19 +0800

    add user signup & flash message

---
+++ controllers/userController.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const User = db.User
+
+const userController = {
+    signUpPage: (req, res) => {
+        res.render('signup')
+    },
+    signUp: (req, res) => {
+      const { name, email, password } = req.body
+      const errors = []
+      User.findOne({ where: { email } })
+      .then(user => {
+        if (user) {
+          errors.push({ message: '信箱已重複註冊!' })
+          res.render('signup', { errors, name, email, password })
+        } else {
+          req.flash('success_messages', '註冊成功!')
+          return User.create({
+            name,
+            email,
+            password: bcrypt.hashSync(password, 10)
+          })
+          .then((user) => res.redirect('/signin'))
+        }
+      })
+  }
+}
+
+module.exports = userController
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,20 @@ const express = require('express')
 const router = express.Router()
 
 const characterController = require('../controllers/characterController')
+const userController = require('../controllers/userController')
+
+const { generalErrorHandler } = require('../middleware/error-handler')
+
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)//收到有關/admin的路徑一律給後臺專用modules處理
+
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
+
 router.get('/characters', characterController.getCharacters)
 router.use('/', (req, res) => res.redirect('/characters'))
+
+router.use('/', generalErrorHandler)
 
 module.exports = router
