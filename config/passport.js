25566ff5d0c62d16fb290085bf4b89dbb4639a30
Zinyang <tyulkeds159357@gmail.com>
2022-10-04 18:28:07 +0800

    modify passport & signin page

--- config/passport.js
+++ config/passport.js
@@ -27,10 +27,10 @@ passport.serializeUser((user, cb) => {
     cb(null, user.id)
 })
 passport.deserializeUser((id, cb) => {
-    User.findByPK(id).then(user => {
-        user = user.toJSON()
-        return cb(null, user)
-    })
+    User.findByPk(id).then(user => {
+            user = user.toJSON()
+            return cb(null, user)
+        })
 })
 
 module.exports = passport
