cfc46358c8070fa587659c0d937b605b4572d637
Zinyang <tyulkeds159357@gmail.com>
2022-10-28 18:11:46 +0800

    add user seed data

---
+++ seeders/20221027085829-users-seed-file.js
@@ -0,0 +1,26 @@
+'use strict';
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Users', [{
+      email: 'root@example.com',
+      password: await bcrypt.hash('123456', 10),
+      name: 'root',
+      is_admin: true,
+      created_at: new Date(),
+      updated_at: new Date()
+    },{
+      email: 'user@example.com',
+      password: await bcrypt.hash('123456', 10),
+      name: 'user',
+      is_admin: false,
+      created_at: new Date(),
+      updated_at: new Date()
+    }], {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', {})
+  }
+};
