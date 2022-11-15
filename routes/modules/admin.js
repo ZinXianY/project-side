cfa78b18ea42e23f2742d0800b36fd8d34b250fc
Zinyang <tyulkeds159357@gmail.com>
2022-11-15 20:20:53 +0800

    add category create & category delete

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 const adminController = require('../../controllers/adminController')
+const categoryController = require('../../controllers/categoryController')
+
 const upload = require('../../middleware/multer')
 const cpUpload = upload.fields([{ name: 'avatar', maxCount: 1}, { name: 'image', maxCount: 1 }])
 
@@ -12,6 +14,11 @@ router.delete('/characters/:id', adminController.deleteCharacter)
 router.get('/characters/:id', adminController.getCharacter)
 router.get('/characters',  adminController.getCharacters)
 router.post('/characters', cpUpload, adminController.postCharacter)
+
+router.delete('/categories/:id', categoryController.deleteCategory)
+router.get('/api/categories', categoryController.createCategory)
+router.post('/api/categories', categoryController.postCategory)
+
 router.use('/', (req, res) => res.redirect('/admin/characters'))
 
 module.exports = router
