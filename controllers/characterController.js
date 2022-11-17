a122ab90f7c49729f91e9c0c53b4d6c0f5623851
Zinyang <tyulkeds159357@gmail.com>
2022-11-17 17:48:24 +0800

    add categories navbar on user character page

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -2,17 +2,26 @@ const { Character, Category } = require('../models')
 
 const characterController = {
     getCharacters: (req, res) => {
-        Character.findAll({
-            raw: true,
-            nest: true,
-            include: [Category]
-        })
-        .then(characters => {
+        const categoryId = Number(req.query.categoryId) || ''
+        const where = {}
+        if (categoryId) where.categoryId = categoryId
+        Promise.all([
+            Character.findAll({
+                raw: true,
+                nest: true,
+                include: [Category],
+                where: where
+            }),
+            Category.findAll({ raw: true })
+        ])
+        .then(([characters, categories]) => {
             const data = characters.map(r => ({
                 ...r
             }))
             return res.render('characters', {
-                characters: data
+                characters: data,
+                categories,
+                categoryId
             })
         })
     },
