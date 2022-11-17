484d5d9796e6c2ae9064147a9a1a246450f43bcc
Zinyang <tyulkeds159357@gmail.com>
2022-11-17 18:02:20 +0800

    add pagination on user characters page

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -1,29 +1,40 @@
 const { Character, Category } = require('../models')
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
 
 const characterController = {
     getCharacters: (req, res) => {
+        const DEFAULT_LIMIT = 8
         const categoryId = Number(req.query.categoryId) || ''
+        //Pagination
+        const page = Number(req.query.page) || 1
+        const limit = Number(req.query.limit) || DEFAULT_LIMIT
+        const offset = getOffset(limit, page)
+
         const where = {}
         if (categoryId) where.categoryId = categoryId
+
         Promise.all([
-            Character.findAll({
+            Character.findAndCountAll({
                 raw: true,
                 nest: true,
                 include: [Category],
-                where: where
+                where: where,
+                limit,
+                offset
             }),
             Category.findAll({ raw: true })
         ])
-        .then(([characters, categories]) => {
-            const data = characters.map(r => ({
-                ...r
-            }))
-            return res.render('characters', {
-                characters: data,
-                categories,
-                categoryId
+            .then(([characters, categories]) => {
+                const data = characters.rows.map(r => ({
+                    ...r
+                }))
+                return res.render('characters', {
+                    characters: data,
+                    categories,
+                    categoryId,
+                    pagination: getPagination(limit, page, characters.count)
+                })
             })
-        })
     },
     getCharacter: (req, res, next) => {
         return Character.findByPk(req.params.id, {
@@ -31,10 +42,10 @@ const characterController = {
             nest: true,
             include: [Category]
         })
-        .then(character => {
-            res.render('character', { character })
-        })
-        .catch(err => next(err))
+            .then(character => {
+                res.render('character', { character })
+            })
+            .catch(err => next(err))
     }
 }
 
