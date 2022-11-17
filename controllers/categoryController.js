4d8cf6a35c828649378784118fd9cc5dd3cf3ac8
Zinyang <tyulkeds159357@gmail.com>
2022-11-17 17:47:10 +0800

    modify category controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -5,28 +5,28 @@ const categoryController = {
     Category.findAll({
       raw: true
     })
-    .then(categories => {
-      const { name } = categories.toJSON()
-      res.json({ name })
-    })
-    .catch(err => next(err))
+      .then(categories => {
+        const { name } = categories.toJSON()
+        res.json({ name })
+      })
+      .catch(err => next(err))
   },
   postCategory: (req, res, next) => {
     const { name } = req.body
     return Category.create({ name })
-    .then(() => {
-      req.flash('success_messages', '新增成功!')
-      res.redirect('back')
-    })
-    .catch(err => next(err))
+      .then(() => {
+        req.flash('success_messages', '新增成功!')
+        res.redirect('back')
+      })
+      .catch(err => next(err))
   },
   deleteCategory: (req, res, next) => {
     return Category.findByPk(req.params.id)
-    .then(category => {
-      return category.destroy()
-    })
-    .then(() => res.redirect('/admin/characters'))
-    .catch(err => next(err))
+      .then(category => {
+        return category.destroy()
+      })
+      .then(() => res.redirect('/admin/characters'))
+      .catch(err => next(err))
   }
 }
 
