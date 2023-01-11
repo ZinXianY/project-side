4eb7029716295ca2043f54b0e73a0701509cfead
Zinyang <tyulkeds159357@gmail.com>
2023-01-11 17:44:22 +0800

    api signin add jwt token

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../../config/passport')
 
 const characterController = require('../../controllers/apis/characterController')
+const userController = require('../../controllers/apis/userController')
 
+//user signin route
+router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
 //user characters page route
 router.get('/characters', characterController.getCharacters)
 //user character page route
