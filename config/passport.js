bfe625fdd84633cd37855cd65db9be27c52836b4
Zinyang <tyulkeds159357@gmail.com>
2023-01-12 18:17:00 +0800

    add api-auth middleware

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,12 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
+const passportJWT = require('passport-jwt')
 const FacebookStrategy = require('passport-facebook').Strategy
 const bcrypt = require('bcryptjs')
 const { User, Character } = require('../models')
+//JWT
+const JWTStrategy = passportJWT.Strategy
+const ExtractJWT = passportJWT.ExtractJwt
 
 //設定 Passport strategy
 passport.use(new LocalStrategy(
@@ -21,6 +25,21 @@ passport.use(new LocalStrategy(
             })
     }
 ))
+
+//JWT
+const jwtOptions = {
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET
+}
+passport.use(new JWTStrategy(jwtOptions, (jwtPayload, cb) => {
+    User.findByPk(jwtPayload.id, {
+        include: [
+            { model: Character, as: 'LikedCharacters' }
+        ]
+    })
+        .then(user => cb(null, user))
+        .catch(err => cb(err))
+}))
 
 //Passport Facebook
 passport.use(new FacebookStrategy({
--- routes/apis/index.js
+++ routes/apis/index.js
@@ -3,21 +3,23 @@ const router = express.Router()
 const passport = require('../../config/passport')
 
 const admin = require('./modules/admin')
+
 const characterController = require('../../controllers/apis/characterController')
 const userController = require('../../controllers/apis/userController')
 
-//user signin route
-router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
+const { authenticated, authenticatedAdmin } = require('../../middleware/api-auth')
 const { apiErrorHandler } = require('../../middleware/error-handler')
 
 //收到有關/admin的路徑一律給後臺專用modules處理
-router.use('/admin', admin)
+router.use('/admin', authenticated, authenticatedAdmin, admin)
 //user characters page route
-router.get('/characters', characterController.getCharacters)
+router.get('/characters', authenticated, characterController.getCharacters)
 //user character page route
-router.get('/characters/:id', characterController.getCharacter)
+router.get('/characters/:id', authenticated, characterController.getCharacter)
 //top characters route
-router.get('/characters/top', characterController.getTopCharacters)
+router.get('/characters/top', authenticated, characterController.getTopCharacters)
+//user signin route
+router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
 
 router.use('/', apiErrorHandler)
 
